dd819df999d62d9264a7d24699fdd55b668548d2
blaffster <50337082+blaffster@users.noreply.github.com>
2020-09-08 16:33:46 -0400

    New functions/density prescription

--- SPH_Attempt_2.py
+++ SPH_Attempt_2.py
@@ -6,11 +6,15 @@ Created on Mon Aug 24 09:54:14 2020
 """
 
 import numpy as np
+import math
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
-from scipy import interpolate
 from scipy.optimize import fsolve
 pi = np.pi
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 14}
+plt.rc('font', **font)
 
 
 # Function to get rho_NFW
@@ -30,6 +34,19 @@ def M_NFW(r,r_s,rho_s):
         return 4*np.pi*rho_s*r_s**3*((1/2)*X**2 - (2/3)*X**3 + (3/4)*X**4 - (4/5)*X**5)
 
 
+# Function to get P_NFW_integrand
+#------------------------------------------------------------------------------
+def P_NFW_integrand(r,r_s,rho_s,G = 4.302e-06):
+    return G * M_NFW(r,r_s,rho_s) * rho_NFW (r,r_s,rho_s) * r**(-2)
+
+
+# Function to get P_NFW
+#------------------------------------------------------------------------------
+def P_NFW(r,r_s,rho_s):
+    integral = quad(P_NFW_integrand, r, math.inf, args=(r_s,rho_s))[0]
+    return integral
+
+
 # Functions to calculate C(q) and D(q)
 #------------------------------------------------------------------------------
 def C(q):
@@ -46,7 +63,7 @@ def W_3S1(rp,r,h):
     σ_prime = rp/h
     X = σ_prime + σ
     Y = abs(σ_prime - σ)
-    print('X=',X,'Y=',Y)
+#    print('X=',X,'Y=',Y)
     prefactor = 1/(h*rp*r)
     
     if 2 < X:
@@ -54,22 +71,24 @@ def W_3S1(rp,r,h):
             return prefactor * ( (7/10) - C(Y) )
         elif 1 <= Y < 2:
             return prefactor * ( (8/10) - D(Y) )
+        else:
+            return 0
         
-    if 1 < X <= 2:
+    elif 1 < X <= 2:
         if 0 <= Y < 1:
             return prefactor * ( (-1/10) + D(X) - C(Y) )
         elif 1 <= Y < 2:
             return prefactor * ( D(X) - D(Y) )
+        else:
+            return 0
         
-    if X <= 1:
+    elif X <= 1:
         return prefactor * ( C(X) - C(Y) )
-    
-    return 0
     
 
 # Function to calculate dW/dr
 #------------------------------------------------------------------------------   
-def dWdr(r,rp,h):
+def dWdr(rp,r,h):
     σ = r/h
     σ_prime = rp/h
     X = σ_prime + σ
@@ -107,6 +126,46 @@ def dWdr(r,rp,h):
                 return -( (2*h-r+rp)**3 * (2*h**2+8*r**2+3*h*(r-rp)-6*r*rp-2*rp**2) )/( 20 * h**6 * r**2 * rp )
 
 
+# Function to calculate dW/drp
+#------------------------------------------------------------------------------
+def dWdrp(rp,r,h):
+    σ = r/h
+    σ_prime = rp/h
+    X = σ_prime + σ
+    Y = abs(σ_prime - σ)
+    Z = σ_prime - σ
+    
+    if X <= 1:
+        if Z > 0:
+            return -(3 * (r**4-10*r**2*rp**2+5*(4*h-3*rp)*rp**3))/(5*h**6*rp**2)
+        else:
+            return (12*rp*(-5*h*r+5*r**2+rp**2))/(5*h**6*r)
+
+    elif 1 < X <= 2:
+        if 0 <= Y < 1:
+            if Z > 0:
+                return (h**5 + 20*h**2*(r-2*rp)*(r+rp)**2 - 10*h**3*(r**2-rp**2) - 15*h*(r**4-6*r**2*rp**2-3*rp**4) - 2*(r**5-10*r**3*rp**2+40*r**2*rp**3-15*r*rp**4+8*rp**5))/(10*h**6*r*rp**2)
+            else:
+                return (h**5 + 20*h**2*(r-2*rp)*(r+rp)**2 - 10*h**3*(r**2-rp**2) - 15*h*(r**4-6*r**2*rp**2-3*rp**4) + 4*(r**5-10*r**3*rp**2+10*r**2*rp**3-15*r*rp**4+2*rp**5))/(10*h**6*r*rp**2)
+        elif 1 <= Y < 2:
+            if Z > 0:
+                return (r**4 - 10*r**2*rp**2 + 60*h*rp**3 - 15*rp**4 + 20*h**2*(r**2-3*rp**2))/(5*h**6*rp**2)
+            else:
+                return (4*rp*(10*h**2-15*h*r+5*r**2+rp**2))/(5*h**6*r)
+    
+    elif 2 < X:
+        if 0 <= Y < 1:
+            if Z > 0:
+                return -(14*h**5 + 15*h*(r-rp)**3*(r+3*rp) + 6*(r-rp)**4*(r+4*rp) - 20*h**3*(r**2-rp**2))/(20*h**6*r*rp**2)
+            else:
+                return (-14*h**5 - 15*h*(r-rp)**3*(r+3*rp) + 6*(r-rp)**4*(r+4*rp) + 20*h**3*(r**2-rp**2))/(20*h**6*r*rp**2)
+        elif 1 <= Y < 2:
+            if Z > 0:
+                return -((2*h+r-rp)**3*(2*h**2-3*h*r-2*r**2+3*h*rp-6*r*rp+8*rp**2))/(20*h**6*r*rp**2)
+            else:
+                return ((2*h-r+rp)**3*(2*h**2+3*h*r-2*r**2-3*h*rp-6*r*rp+8*rp**2))/(20*h**6*r*rp**2)
+
+
 # Function to get p_s
 #------------------------------------------------------------------------------
 def get_rho_s(c,p_crit):
@@ -140,12 +199,38 @@ def r_func(r,M,r_s,rho_s):
 def rho_i(r,h,m,N):
     rho = np.zeros(N)
     for i in range(0,N):
-        rho[i] = m * W_3S1(r[i],r[i],h)
+        rho[i] = m * W_3S1(r[i],r[i],h[i])
         for j in range(i+1,N):
-            rho_ij = m * W_3S1(r[j],r[i],h)
+            rho_ij = m * W_3S1(r[j],r[i],h[i])
             rho[i] += rho_ij
             rho[j] += rho_ij
     return rho
+
+
+# Function used to determine dvdt
+#------------------------------------------------------------------------------
+def dvdt(m,P,rho,r,G=4.302e-6):
+    RHS = np.zeros(N)
+    for i in range(0,N):
+        for j in range(0,N):
+            if j != i:
+                term1 = -m * ( (P[i]/rho[i]**2) + (P[j]/rho[j]**2) ) * dWdr(r[j],r[i],h)
+                if r[i] > r[j]:
+                    term2 = -G * (m/r[j]**2)
+                else:
+                    term2 = 0
+                RHS[i] += term1 + term2
+    return RHS
+
+
+# Function used to determine dudt
+#------------------------------------------------------------------------------
+def dudt(m,P,rho,v):
+    RHS = np.zeros(N)
+    for i in range(0,N):
+        for j in range(0,N):
+            if j != i:
+                RHS[i] += m * (P[i]/rho[i]**2) * ( v[j]*dWdrp(r[j],r[i],h[i]) + v[i]*dWdr(r[j],r[i],h[i]) )
 
 
 
@@ -171,22 +256,46 @@ for i in range(0,N):
 M_samples = np.sort(M_samples)
 
 r = np.zeros(N)
+v = np.zeros(N)
+P = np.zeros(N)
 for i in range(0,N):
     r[i] = fsolve(r_func, 1, args=(M_samples[i],r_s,rho_s))[0]
-#plt.loglog(r,[M_NFW(x,r_s,rho_s) for x in r])
-#plt.loglog(r,M_samples,'.')
+    P[i] = P_NFW(r[i],r_s,rho_s)
+#plt.clf()
+#plt.loglog(r,[M_NFW(x,r_s,rho_s) for x in r],label='NFW')
+#plt.loglog(r,M_samples,'.',label='SPH')
+#plt.xlabel('r')
+#plt.ylabel('M(r)')
+#plt.legend()
 #plt.show()
 
+plt.clf()
 particle_distances = []
 for i in range(0,N):
     for j in range(i+1,N):
         particle_distances.append(np.abs(r[j]-r[i]))
 avg_dist = sum(particle_distances)/len(particle_distances)
-fraction = 0.25
-h = fraction * avg_dist
-
+fraction = 0.2
+sl = fraction * avg_dist
+h = np.ones(N) * sl
 rho = rho_i(r,h,m,N)
+plt.loglog(r,rho,'.',label='SPH, h='+str(round(sl,2)))
 
-plt.loglog(r,[rho_NFW(x,r_s,rho_s) for x in r])
-plt.loglog(r,rho,'.')
+h = np.zeros(N)
+eta = 1
+while eta<10:
+    for i in range(0,N):
+        h[i] = eta * (m/rho_NFW(r[i],r_s,rho_s))**(1/3)
+    rho = rho_i(r,h,m,N)
+    plt.loglog(r,rho,'.',label='SPH, η='+str(eta))
+    eta += 2
+
+plt.loglog(r,[rho_NFW(x,r_s,rho_s) for x in r],'k-',label='NFW')
+plt.xlabel('r')
+plt.ylabel('ρ(r)')
+plt.legend()
 plt.show()
+
+#e = np.zeros(N)
+#for i in range(0,N):
+#    e[i] = (3/2)*(P[i]/rho[i])
