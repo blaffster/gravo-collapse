237a259cd977153fee42efbcf29fe48fe2cd4a47
blaffster <50337082+blaffster@users.noreply.github.com>
2020-09-08 16:34:48 -0400

    Added plot of integrand/fixed integration

--- Omang_Eq_10.py
+++ Omang_Eq_10.py
@@ -5,6 +5,8 @@ Created on Wed Sep  2 09:17:55 2020
 @author: nebue
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.integrate import quad
 import math
 
@@ -50,12 +52,53 @@ def W3_integrand(rp,r,h):
     else:
         return 0
 
-h = 1
-r = (7/2) * h
-epsilon = 1e-12
 
-integral_1 = quad(W1_integrand, epsilon, h-r, args=(r,h))[0]
-integral_2 = quad(W2_integrand, h-r+epsilon, 2*h-r, args=(r,h))[0]
-integral_3 = quad(W3_integrand, 2*h-r+epsilon, math.inf, args=(r,h))[0]
-integral_tot = integral_1 + integral_2+ integral_3
-print(integral_tot)
+plt.clf()
+epsilon = 1e-12
+factor = 9
+h = 1
+r = (9/2) * h
+X = h-r
+Y = 2*h-r
+
+if X > 0:
+    print('Case 1')
+    rp1 = np.linspace(epsilon,X,500)
+    rp2 = np.linspace(X+epsilon,Y,500)
+    rp3 = np.linspace(Y+epsilon,factor*h,500)
+    W1 = [W1_integrand(x,r,h) for x in rp1]
+    W2 = [W2_integrand(x,r,h) for x in rp2]
+    W3 = [W3_integrand(x,r,h) for x in rp3]
+    plt.plot(rp1,W1,label='W1')
+    plt.plot(rp2,W2,label='W2')
+    plt.plot(rp3,W3,label='W3')
+    integral_1 = quad(W1_integrand, epsilon, X, args=(r,h))[0]
+    integral_2 = quad(W2_integrand, X+epsilon, Y, args=(r,h))[0]
+    integral_3 = quad(W3_integrand, Y+epsilon, math.inf, args=(r,h))[0]
+    integral_tot = integral_1 + integral_2 + integral_3
+    print(integral_tot)
+    
+elif Y > 0:
+    print('Case 2')
+    rp2 = np.linspace(epsilon,Y,500)
+    rp3 = np.linspace(Y+epsilon,factor*h,500)
+    W2 = [W2_integrand(x,r,h) for x in rp2]
+    W3 = [W3_integrand(x,r,h) for x in rp3]
+    plt.plot(rp2,W2,label='W2')
+    plt.plot(rp3,W3,label='W3')
+    integral_2 = quad(W2_integrand, epsilon, Y, args=(r,h))[0]
+    integral_3 = quad(W3_integrand, Y+epsilon, math.inf, args=(r,h))[0]
+    integral_tot = integral_2 + integral_3
+    print(integral_tot)
+    
+elif Y <= 0:
+    print('Case 3')
+    rp3 = np.linspace(epsilon,factor*h,500)
+    W3 = [W3_integrand(x,r,h) for x in rp3]
+    plt.plot(rp3,W3,label='W3')
+    integral_3 = quad(W3_integrand, epsilon, math.inf, args=(r,h))[0]
+    integral_tot = integral_3
+    print(integral_tot)
+    
+plt.legend()
+plt.show()
