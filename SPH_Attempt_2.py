Fixed missing factor of 2 in smoothing length in np.where()

SPH_Attempt_2.py
@@ -264,65 +264,39 @@ def r_func(r,M,r_s,rho_s):
 
 # Function used to determine rho_i
 #------------------------------------------------------------------------------
-def get_rho(N,i,j,r,h,m):
+def get_rho(N,i,index,r,h,m):
     rho_ij = 0
-    index = np.where(abs(r[i]-r[i,j]) <= h)[0]
-    for k in index:
-        rho_ij += m * W_3S1(r[i,k],r[i,j],h)
+    for j in index:
+        rho_ij += m * W_3S1(r[j],r[i],h)
     return rho_ij
-
-
-#def get_rho(N,i,r,h,m):
-#    rho = 0
-#    for j in range(0,N):
-#        rho += m * W_3S1(r[j],r[i],h[i])
-#    return rho
 
 
 # Function used to determine dvdt
 #------------------------------------------------------------------------------
-def dvdt(N,i,m,P,rho,r,h,G=4.302e-6):
+def dvdt(N,i,index,m,P,rho,r,h,G=4.302e-6):
     RHS = 0
-    for j in range(0,N):
+    force = 0
+    pressure = 0
+    for j in index:
         if j != i:
-            term1 = -m * ( (P[i]/rho[i]**2) + (P[j]/rho[j]**2) ) * dWdr(r[j],r[i],h[i])
+            term1 = -m * ( (P[i]/rho[i]**2) + (P[j]/rho[j]**2) ) * dWdr(r[j],r[i],h)
+            pressure += term1
             if r[i] > r[j]:
                 term2 = -G * (m/r[i]**2)
+                force += term2
             else:
                 term2 = 0
             RHS += term1 + term2
-    return RHS
-
-
-#def force_term(N,i,m,r,G=4.302e-6):
-#    RHS = 0
-#    for j in range(0,N):
-#        if j != i:
-#            if r[i] > r[j]:
-#                term2 = -G * (m/r[j]**2)
-#            else:
-#                term2 = 0
-#            RHS += term2
-#    return RHS
-#
-#
-#def pressure_term(N,i,m,P,rho,r,h,G=4.302e-6):
-#    RHS = 0
-#    for j in range(0,N):
-#        if j != i:
-#            term1 = -m * ( (P[i]/rho[i]**2) + (P[j]/rho[j]**2) ) * dWdr(r[j],r[i],h[i])
-#            RHS += term1
-#    return RHS
+    return RHS, force, pressure
     
-
 
 # Function used to determine dudt
 #------------------------------------------------------------------------------
-def dedt(N,i,m,P,rho,v,h,r):
+def dedt(N,i,index,m,P,rho,v,h,r):
     RHS = 0
-    for j in range(0,N):
+    for j in index:
         if j != i:
-            RHS += m * (P[i]/rho[i]**2) * ( v[j]*dWdrp(r[j],r[i],h[i]) + v[i]*dWdr(r[j],r[i],h[i]) )
+            RHS += m * (P[i]/rho[i]**2) * ( v[j]*dWdrp(r[j],r[i],h) + v[i]*dWdr(r[j],r[i],h) )
     return RHS
 
 
@@ -345,17 +319,12 @@ R_vir = c*r_s
 # Set number of particles/particle mass
 #------------------------------------------------------------------------------
 M_vir = M_NFW(R_vir,r_s,rho_s)
-N = 5000
+N = 1000
 m = 1/(4*pi) * (M_vir/N)
 
 
 # Sample initial density distribution, initialize r_i/v_i/P_i
 #------------------------------------------------------------------------------
-#M_samples = np.zeros(N)
-#np.random.seed(2)
-#for i in range(0,N):
-#    M_samples[i] = np.random.uniform()*M_vir
-#M_samples = np.sort(M_samples)
 M_samples = np.linspace(M_vir/N,M_vir,N)
 steps = 15
 r = np.zeros((steps+1,N))
@@ -368,39 +337,33 @@ for j in range(0,N):
 
 # Initialize smoothing lengths h_i
 #------------------------------------------------------------------------------
-eta_new = 0.7
-#eta = 1.5
+eta_new = 2.5
 h = np.zeros((steps+1,N))
 for j in range(N):
     h[0,j] = eta_new * (m)/(r[0,j]**2 * rho_NFW(r[0,j],r_s,rho_s))
-#    h[0,j] = eta * (m/rho_NFW(r[0,j],r_s,rho_s))**(1/3)
 
 
 # Calculate/plot SPH density profile, compare it to NFW, initialize u
 #------------------------------------------------------------------------------
 rho = np.zeros((steps+1,N))
-neighbors = np.zeros(N)
-for i in range (0,N):
-    index = np.where(abs(r[0]-r[0,i]) <= h[0,i])[0]
-    neighbors[i] = len(index)
-    for j in index:
-        rho[0,i] += m * W_3S1(r[0,j],r[0,i],h[0,i])
-np.savetxt('neighbors.csv', neighbors, delimiter=',')
-    
-#rho = np.zeros((steps+1,N))
-#for j in range(0,N):
-#    rho[0,j] = get_rho(N,j,r[0],h[0],m)
+neighbors = np.zeros((steps,N))
+for j in range (0,N):
+    index = np.where(abs(r[0]-r[0,j]) <= 2*h[0,j])[0]
+    neighbors[0,j] = len(index)
+    for k in index:
+        rho[0,j] += m * W_3S1(r[0,k],r[0,j],h[0,j])
 e = np.zeros((steps+1,N))
 e[0] = (3/2)*(P[0]/rho[0])
 
+
+print(min(neighbors[0]),max(neighbors[0]),np.average(neighbors[0]))
 #plt.clf()
 #plt.loglog(r[0],rho[0],'.',label='SPH (η='+str(eta_new)+')')
 #plt.loglog(r[0],[rho_NFW(x,r_s,rho_s) for x in r[0]],label='NFW')
-#plt.plot(r[0],neighbors,'.',label='# of neighbors')
+#plt.plot(r[0],neighbors[0],'.',label='# of neighbors')
 #plt.title('N='+str(N))
 #plt.title('NFW vs. SPH Initial Density Profile (N='+str(N)+')')
 #plt.xlabel('r  [kpc]')
-#plt.ylabel('ρ(r)  [M_sun/kpc^3]')
 #plt.legend()
 #plt.show()
 
@@ -409,69 +372,41 @@ e[0] = (3/2)*(P[0]/rho[0])
 #------------------------------------------------------------------------------
 dt = 0.001
 t_elapsed = 0
-print('t_elapsed =',t_elapsed,'  r_0 =', r[0,0],'  rho_0 =',rho[0,0])
-#forces = np.zeros(steps+1)
-#pressures = np.zeros(steps+1)
-#energy = np.zeros(steps+1)
-#forces[0] = force_term(N,0,m,r[0])
-#pressures[0] = pressure_term(N,0,m,P[0],rho[0],r[0],h[0])
-#energy[0] = dedt(N,0,m,P[0],rho[0],v[0],h[0],r[0])
+forces = np.zeros((steps,N))
+pressures = np.zeros((steps,N))
+p_index = 0
+print('step =',0,'  r_0 =', "{:.4f}".format(r[0,p_index]),'  rho_0 =',"{:e}".format(rho[0,p_index]),'  h_0 =',"{:.3f}".format(h[0,p_index]),sep = '\t')
 for i in range (0,steps):
     for j in range(0,N):
 #         Update r, v, and u
-        v[i+1,j] = v[i,j] + dvdt(N,j,m,P[i],rho[i],r[i],h[i])*dt
+        index = np.where(abs(r[i]-r[i,j]) <= 2*h[i,j])[0]
+        neighbors[i,j] = len(index)
+        a, forces[i,j], pressures[i,j] = dvdt(N,j,index,m,P[i],rho[i],r[i],h[i,j])
+        v[i+1,j] = v[i,j] + a*dt
         r[i+1,j] = r[i,j] + v[i,j]*dt
-        e[i+1,j] = e[i,j] + dedt(N,j,m,P[i],rho[i],v[i],h[i],r[i])*dt
-        print(r[i,j])
+        e[i+1,j] = e[i,j] + dedt(N,j,index,m,P[i],rho[i],v[i],h[i,j],r[i])*dt
     for j in range(0,N):
 #         Update rho, P, and h
-        rho[i+1,j] = get_rho(N,i,j,r[i+1],h[i,j],m)
+        index = np.where(abs(r[i]-r[i,j]) <= 2*h[i,j])[0]
+        rho[i+1,j] = get_rho(N,j,index,r[i+1],h[i,j],m)
         P[i+1,j] = (2/3) * rho[i+1,j] * e[i+1,j]
-        h[i+1,j] = eta_new * (m/rho[i+1,j])**(1/3)
         h[i+1,j] = eta_new * (m)/(r[i+1,j]**2 * rho[i+1,j])
-#    forces[i+1] = force_term(N,0,m,r[i+1])
-#    pressures[i+1] = pressure_term(N,0,m,P[i+1],rho[i+1],r[i+1],h[i+1])
-#    energy[i+1] = dedt(N,0,m,P[i+1],rho[i+1],v[i+1],h[i+1],r[i+1])
     t_elapsed += dt
-    print('t_elapsed =',t_elapsed,'  r_0 =', r[i+1,0],'  rho_0 =',rho[i+1,0])
+    print('step =',i+1,'  r_0 =', "{:.4f}".format(r[i+1,p_index]),'  rho_0 =',"{:e}".format(rho[i+1,p_index]),'  h_0 =',"{:.3f}".format(h[i+1,p_index]),sep = '\t')
 
 
-#np.savetxt('v.csv', v, delimiter=',')
-#np.savetxt('r.csv', r, delimiter=',') 
-#np.savetxt('e.csv', e, delimiter=',') 
-#np.savetxt('rho.csv', rho, delimiter=',') 
-#np.savetxt('P.csv', P, delimiter=',') 
-#np.savetxt('h.csv', h, delimiter=',') 
+np.savetxt('neighbors.csv', neighbors, delimiter=',')
 
 
-#plt.clf()
+plt.clf()
 #plt.ylabel('r [kpc]')
 #plt.xlabel('time step')
 #plt.title('Positions of 10 innermost particle-shells vs. time')
-#
-#for j in range(10):
+
+for j in range(10):
 #    plt.plot(r[0:steps,j],linestyle='--', marker='o',markersize=3,label='Particle '+str(j))
-#    
-#plt.legend(loc='upper right', bbox_to_anchor=(1.1, 1))
-#plt.show()
-
-#plt.title('SPH Density Profile Evolution (N=1000,steps=15,dt=0.001)')
-#plt.xlabel('r  [kpc]')
-#plt.ylabel('ρ(r)  [M_sun/kpc^3]')
-#r_vals = np.linspace(min(r[-1]),max(r[-1]),1000)
-#plt.loglog(r_vals,[rho_NFW(x,r_s,rho_s) for x in r_vals],label='NFW')
-#plt.loglog(r[0],rho[0],linestyle='--', marker='o',markersize=3,label='Initial SPH')
-#plt.loglog(r[-1],rho[-1],linestyle='--', marker='o',markersize=3,label='Final SPH')
-
-#plt.plot(r[:,0],linestyle='--', marker='o',markersize=3)
-#plt.title('Position of innermost particle-shell vs. time')
-#plt.ylabel('r [kpc]')
-#plt.plot(forces,linestyle='--', marker='o',markersize=3,label='Force')
-#plt.plot(pressures,linestyle='--', marker='o',markersize=3,label='Pressure')
-#plt.plot(energy,linestyle='--', marker='o',markersize=3,label='Energy term')
-#plt.title('Force/pressure term for innermost particle-shell vs. time')
-#plt.xlabel('time step')
-#plt.ylabel('force/pressure term')
-
-#plt.legend()
-#plt.show()
+    plt.plot(forces[0:steps,j],linestyle='--', marker='o',markersize=3,label='Particle '+str(j)+' force')
+#    plt.plot(pressures[0:steps,j],linestyle='--', marker='o',markersize=3,label='Particle '+str(j)+' pressure')
+    
+plt.legend(loc='upper right', bbox_to_anchor=(1.1, 1))
+plt.show()